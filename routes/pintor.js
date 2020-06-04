3296981d67e8fc9a08b98d37d4d9c459fabca66f
Ydani25 <65005801+Ydani25@users.noreply.github.com>
2020-06-03 22:42:01 -0600

    Add files via upload

---
+++ routes/pintor.js
@@ -0,0 +1,11 @@
+const express = require('express');
+//Vamos a routear a los pintores
+let pintor = require('../controllers/PintoresController.js');
+
+//creamos nuetras rutas
+let router = express.Router();
+
+router.get('/', pintor.list);
+
+module.exports = router;
+
